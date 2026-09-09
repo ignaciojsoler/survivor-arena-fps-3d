a2ee0edcd2b30f83eccb9ce8a4c7f6bee90a1410
Ignacio Soler <ignaciojsoler@gmail.com>
2026-09-09 15:15:42 -0300

    feat: bullet script

---
+++ player/bullet_3d.gd
@@ -0,0 +1,15 @@
+extends Area3D
+
+const SPEED = 10
+const RANGE = 5
+
+var travelled_distance = 0
+
+func _physics_process(delta: float) -> void:
+	# Move the bullet
+	self.position += -transform.basis.z * SPEED * delta
+	
+	# Increase and check travelled distance
+	travelled_distance += SPEED * delta
+	if travelled_distance > RANGE:
+		queue_free()
