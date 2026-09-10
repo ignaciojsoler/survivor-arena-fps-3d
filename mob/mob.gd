6f86d675035ec50cd530550106202f125c7d1f47
Ignacio Soler <ignaciojsoler@gmail.com>
2026-09-10 10:15:43 -0300

    feat: bat follows player pos

--- mob/mob.gd
+++ mob/mob.gd
@@ -1,6 +1,16 @@
 extends RigidBody3D
 
 @onready var bat_model: Node3D = %bat_model
+@onready var player: CharacterBody3D = get_node("/root/Game/Player")
+
+var speed = randf_range(2.0, 4.0)
+
+func _physics_process(delta: float) -> void:
+	var direction = global_position.direction_to(player.global_position)
+	direction.y = 0
+	
+	linear_velocity = direction * speed
+	bat_model.rotation.y = Vector3.FORWARD.signed_angle_to(direction, Vector3.UP) + PI
 
 func take_damage():
 	bat_model.hurt()
